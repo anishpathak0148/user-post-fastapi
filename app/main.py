d64ea26b4b5389d1f7c446cd1bebafc504cef14e
Anish Pathak <anishpathak0148@gmail.com>
2022-08-06 13:10:39 +0530

    Add middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
+import time
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.user import router as user_router
 from app.api.post import router as post_router
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["response_time"] = f"{process_time} sec"
+    return response
 
 app.include_router(user_router)
 app.include_router(post_router)
