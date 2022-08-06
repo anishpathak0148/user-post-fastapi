a8e791210bf78627f20bd4e6a87e611696b8a836
Anish Pathak <anishpathak0148@gmail.com>
2022-08-06 13:19:10 +0530

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,10 +2,24 @@ import time
 from typing import Union
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.user import router as user_router
 from app.api.post import router as post_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
