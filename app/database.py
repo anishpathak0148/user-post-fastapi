815a80f3401e16fed0503584a581f98584a00c74
Anish Pathak <anishpathak0148@gmail.com>
2023-01-30 17:50:25 +0530

    Setup postgres database with docker-compose

--- app/database.py
+++ app/database.py
@@ -1,16 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # Create SqlAlchemy engine
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+# Remove check_same_thread- it is an argument specific to sqlite.
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 # Create a SessionLocal class
