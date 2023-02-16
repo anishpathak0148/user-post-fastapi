1e8103fe3424db3f4ae8dafc214654dbd793c49a
Anish Pathak <anishpathak0148@gmail.com>
2023-02-16 09:48:07 +0530

    Set up Kubernetes deployment for app and postgres db

--- app/database.py
+++ app/database.py
@@ -1,14 +1,29 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DB_USER = os.getenv("POSTGRES_USER")
+SQLALCHEMY_DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+SQLALCHEMY_DB = os.getenv("POSTGRES_DB")
+SQLALCHEMY_DB_HOST = os.getenv("POSTGRES_SERVER")
 
+SQLALCHEMY_DATABASE_URL = URL.create(
+    "postgresql",
+    username=SQLALCHEMY_DB_USER,
+    password=SQLALCHEMY_DB_PASSWORD,  # plain (unescaped) text
+    host=SQLALCHEMY_DB_HOST,
+    port=5432,
+    database=SQLALCHEMY_DB,
+)
+
+print(f"SQLALCHEMY_DATABASE_URL is: {SQLALCHEMY_DATABASE_URL}")
 
 # Create SqlAlchemy engine
 # engine = create_engine(
--- readme.md
+++ readme.md
@@ -6,3 +6,11 @@
 
 ## To generate a secure random secret key:
     openssl rand -hex 32
+
+## To create configmap in kubernetes cluster:
+    kubectl create cm user-post-config --from-env-file=./env/config.env
+## To create secret in kubernetes cluster:
+    kubectl create secret generic user-post-secret --from-env-file=./env/secret.env
+
+## To access postgres inside kuberbetes pod:
+    psql -h localhost -p 5432 -U postgres
